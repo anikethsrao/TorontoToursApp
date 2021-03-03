42df855055e13772442e4f2189f2d7800d2cddb7
Ani <anikethsrao@gmail.com>
2021-03-02 23:29:18 -0500

    feat: tourism files added

--- app/src/main/java/com/example/android/torontotoursapp/RestaurantFragment.java
+++ app/src/main/java/com/example/android/torontotoursapp/RestaurantFragment.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 public class RestaurantFragment extends Fragment {
 
     public RestaurantFragment(){
-
+        //required fragment constructor
     }
 
     @Override
@@ -30,10 +30,13 @@ public class RestaurantFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        //initialize rootView
         View rootView = inflater.inflate(R.layout.fragment_restaurant, container, false);
 
+        //create list of restaurants in toronto
         ArrayList<Toronto> restaurantList = new ArrayList<>();
 
+        //add restaurants to restaurant list
         restaurantList.add(new Toronto(R.drawable.canoe_restaurant, "canoe", "66 Wellington St W 54th floor, Toronto, ON M5K 1H6", "(416) 364-0054", 4.5));
         restaurantList.add(new Toronto(R.drawable.canoe_restaurant, "Scaramouche", "1 Benvenuto Pl, Toronto, ON M4V 2L1", " (416) 961-8011", 4.7));
         restaurantList.add(new Toronto(R.drawable.canoe_restaurant, "Opus", "37 Prince Arthur Ave, Toronto, ON M5R 1B2", "(416) 921-3105", 4.5));
@@ -41,8 +44,11 @@ public class RestaurantFragment extends Fragment {
         restaurantList.add(new Toronto(R.drawable.canoe_restaurant, "GEORGE", "111C Queen St E, Toronto, ON M5C 1S2", "(416) 863-6006", 4.6));
         restaurantList.add(new Toronto(R.drawable.canoe_restaurant, "Ruby Watchco", "730 Queen St E, Toronto, ON M4M 1H2", "(416) 465-0100", 4.6));
 
+        //initialize view adapter
         RestaurantAdapter adapterView = new RestaurantAdapter(getActivity(), restaurantList);
+        //initialize listView
         ListView restaurantListView = rootView.findViewById(R.id.restaurants_list_view);
+        //attach view adapter
         restaurantListView.setAdapter(adapterView);
 
         return rootView;
--- app/src/main/java/com/example/android/torontotoursapp/TourismFragment.java
+++ app/src/main/java/com/example/android/torontotoursapp/TourismFragment.java
@@ -8,57 +8,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
+import static com.example.android.torontotoursapp.R.drawable.canoe_restaurant;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link TourismFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class TourismFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public TourismFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TourismFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TourismFragment newInstance(String param1, String param2) {
-        TourismFragment fragment = new TourismFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tourism, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_tourism, container, false);
+
+        //create list of tourist attractions in toronto
+        ArrayList<Toronto> tourismList = new ArrayList<>();
+
+        tourismList.add(canoe_restaurant, "CN Tower", "290 Bremner Blvd", "(416) 601-4707", "https://www.cntower.ca/intro.html", 4.6);
+        tourismList.add(canoe_restaurant, "CN Tower", "290 Bremner Blvd", "(416) 601-4707", "https://www.cntower.ca/intro.html", 4.6);
+
+        return rootView;
+
     }
 }
