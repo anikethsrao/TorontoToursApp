f6c58e6a05d75650beb096c8c7ed7e0608fa3b53
Ani <anikethsrao@gmail.com>
2021-03-04 10:58:38 -0500

    refactor: switched to ViewPager2

--- app/src/main/java/com/example/android/torontotoursapp/MainActivity.java
+++ app/src/main/java/com/example/android/torontotoursapp/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
@@ -16,18 +18,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // get fragment manager
-        FragmentManager fm = getSupportFragmentManager();
+        //FragmentManager fm = getSupportFragmentManager();
         // add fragment
-        FragmentTransaction ft = fm.beginTransaction();
-
-        //Only run one at a time
+        //FragmentTransaction ft = fm.beginTransaction();
         // restaurant fragment
         // ft.add(R.id.frame_main, new RestaurantFragment(), null);
-        // tourism fragment
-        ft.add(R.id.frame_main, new TourismFragment(), null);
 
-        ft.commit();
+        // Instantiate View Pager
+        ViewPager2 viewPager = findViewById(R.id.pager);
+        viewPager.setAdapter(new ScreenSlideAdapter(getSupportFragmentManager(), getLifecycle()));
 
     }
 }
---
+++ app/src/main/java/com/example/android/torontotoursapp/ScreenSlideAdapter.java
@@ -0,0 +1,37 @@
+package com.example.android.torontotoursapp;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+/**
+ * A simple pager adapter that represents ScreenSlidePageFragment objects, in
+ * sequence.
+ */
+public class ScreenSlideAdapter extends FragmentStateAdapter {
+
+    int COUNT = 2;
+
+    public ScreenSlideAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        if (position == 0) {
+            return new RestaurantFragment();
+        } else { // if (position == 1){
+            return new TourismFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return COUNT;
+    }
+}
